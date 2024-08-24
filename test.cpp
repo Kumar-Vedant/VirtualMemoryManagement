199480861e5e31c2be90bf44cfb819afb4efdbbb
Kumar Vedant <2023aib1009@iitrpr.ac.in>
2024-08-24 12:39:25 +0530

    Implement all modules individually

---
+++ test.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int numberOfTasks = getNumberOfTasks();
+    
+    // generate trace file with the format taskID:logicalAddress:size
+    generateTraceFile(numberOfTasks);
+
+    return 0;
+}
+
+int getNumberOfTasks() 
+{
+    int numberOfTasks;
+    cout << "Enter the number of tasks: ";
+    cin >> numberOfTasks;
+    return numberOfTasks;
+}
+
+void generateTraceFile(int numberOfTasks)
+{
+    ofstream traceFile;
+    traceFile.open("trace.txt");
+    for (int i = 0; i < 1000000; i++)
+    {
+        int taskID = rand() % numberOfTasks + 1;
+
+        int logicalAddress = rand() % 1000;
+
+        // convert to hexadecimal
+        // string logicalAddressHex = to_string(logicalAddress);
+        // logicalAddressHex = "0x" + logicalAddressHex;
+        
+        int size = rand() % 100;
+        
+        traceFile << "T" << taskID << ":" << logicalAddress << ":" << size << endl;
+    }
+    traceFile.close();
+}
